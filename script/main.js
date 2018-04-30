9deb9de41ff08789619278f7c142a16ac4bf468f
marcoSource <gammma7869@gmail.com>
2018-04-30 18:25:45 +0100

    all for today

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
 function goto(html){
   window.location.href = html
 }
+
+function showTextOnHover(){
+  var c = document.getElementsByClassName('container');
+  c.style.visibility = visible;
+  alert(c.style.visibility);
+}
