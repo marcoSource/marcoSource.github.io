aa0832b8332e964d434d01d0d3fc541b2c0aaa82
marcoSource <gammma7869@gmail.com>
2018-06-08 18:36:22 +0100

    done

--- project/viviana_2/script/main.js
+++ project/viviana_2/script/main.js
@@ -1,6 +1,7 @@
 function showModal(imgID){
+  var src = "assets/";
   var modelo = document.getElementById('modal');
-
+  var imgInt = imgID;
   var img = document.getElementById(imgID);
   var modalImg = document.getElementById("img");
   var caption = document.getElementById("caption");
@@ -8,6 +9,15 @@ function showModal(imgID){
       modelo.style.display = "block";
       modalImg.src = this.src;
       caption.innerHTML = this.alt;
+  }
+  onkeypress = function(event){
+    if(modelo.style.display == "block"){
+      if(imgInt < 20)
+        imgInt++;
+      if(imgInt === 20)
+        imgInt = 1;
+      modalImg.src = src + imgInt + ".jpg";
+    }
   }
 }
 
