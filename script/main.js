869163001a7b2ead875c53f8da7b8c0127b42c8a
marcoSource <gammma7869@gmail.com>
2018-10-02 18:31:08 +0100

    Added mobile detection

--- script/main.js
+++ script/main.js
@@ -1,3 +1,25 @@
 function goto(html){
   window.location.href = html
 }
+
+function detectmob() { 
+  if(navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+     return true;
+   }
+  else {
+     return false;
+   }
+ }
+
+ function showDialog(){
+   if(detectmob()){
+     alert('You are using a mobile phone! Keep in mind that the website is build for computers, so you may see weird things.')
+   }
+ }
